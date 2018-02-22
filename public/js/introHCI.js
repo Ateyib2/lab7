1ae354de58c37237cbf67cc3d3255549e1168ca5
Ateyib Abdulkadir <aabdulka@ucsd.edu>
2018-02-22 14:40:47 -0800

    first commit

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -5,9 +5,14 @@ $(document).ready(function() {
 	initializePage();
 });
 
+
 /*
  * Function that is called when the document is ready.
  */
 function initializePage() {
 	// your code here
+	$(".likeBtn").click(function(e) {
+		ga('create', 'UA-114645693-1', 'auto');
+		ga("send", "event", 'like', 'click');
+	});
 }
